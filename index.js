e4c54327cfeb3becce74446131bfa0704e067391
Domenico Flauto <domenicoflauto@Domenicos-MacBook-Pro.local>
2020-12-25 22:23:11 +0100

    adds new route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/dom", (req, res) => {
+  res.send("yeeeeee");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
